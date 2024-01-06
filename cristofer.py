0b2fac31311d377233249dc6fe803be6f948d1d3
vinisipii <khris12319661@gmail.com>
2024-01-05 20:51:37 -0600

    try3

--- cristofer.py
+++ cristofer.py
@@ -25,27 +25,27 @@ global vPosicionX, vPosicionY, nuevo_x, nuevo_y
 global bloqueActual
 
 #prueba matriz 1
-matriz = [[1,2,3,4,5,6,7,8,9,10],
-          [1,2,3,4,5,6,7,8,9,10],
-          [1,2,3,4,5,6,7,8,9,10],
-          [1,2,3,4,5,6,7,8,9,10],
-          [1,2,3,4,5,6,7,8,9,10],
-          [1,2,3,4,5,6,7,8,9,10],
-          [1,2,3,4,5,6,7,8,9,10],
-          [1,2,3,4,5,6,7,8,9,10],
-          [1,2,3,4,5,6,7,8,9,10],
-          [1,2,3,4,5,6,7,8,9,10],
-          [1,2,3,4,5,6,7,8,9,10],
-          [1,2,3,4,5,6,7,8,9,10],
-          [1,2,3,4,5,6,7,8,9,10],
-          [1,2,3,4,5,6,7,8,9,10],
-          [1,2,3,4,5,6,7,8,9,10]]
+matriz = [[0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0]]
 
 
 #prueba matriz 2
-rows = 10
-cols = 20
-matrix = [[0] * cols for _ in range(rows)]
+filas = 10
+columnas = 20
+matrix = [[0 for c in range(columnas)] for f in range(filas)]
 
 
 # Manejo de variables
@@ -69,7 +69,6 @@ vBloqueTamano = vVentanaAncho // vCantidadColumnas
 #  funciones
 ############################################################
 def dibujaCuadricula():
-    
     #intento de recorrer matriz y meter los valores en el x, y.
     prueba = matrix
     filas = len(prueba)
@@ -81,11 +80,36 @@ def dibujaCuadricula():
                 pygame.draw.rect(vVentana, vColor, (x, y, vBloqueTamano, vBloqueTamano), 1 )
     print()
     
+    
+#ficha
+class ficha:
+    x = 0 #x, y para meter en el metodo pygame.draw.rect
+    y = 0
+    figura = [
+        [[1, 5, 9, 13], [4, 5, 6, 7]],
+        [[4, 5, 9, 10], [2, 6, 5, 9]],
+        [[6, 7, 9, 10], [1, 5, 6, 10]],
+        [[1, 2, 5, 9], [0, 4, 5, 6], [1, 5, 9, 8], [4, 5, 6, 10]],
+        [[1, 2, 6, 10], [5, 6, 7, 9], [2, 6, 10, 11], [3, 5, 6, 7]],
+        [[1, 4, 5, 6], [1, 4, 5, 9], [4, 5, 6, 9], [1, 5, 6, 9]],
+        [[1, 2, 5, 6]] #cuadrado
+    ] 
+    
+    #Hasta aqui no sé como implementar las fichas
+    def recorrerficha(): #este metodo ya es capaz de repintar sobre la primer cuadrícula
+        test = ficha.figura
+        for f in range(4):
+            for ff in range(4):
+                x = f * vBloqueTamano
+                y = ff * vBloqueTamano
+                pygame.draw.rect(vVentana, (100, 100, 100), (x, y, vBloqueTamano, vBloqueTamano), 1 )
+
+
+    
 
 ############################################################
 #  inicia el juego
 ############################################################
-
 
 # inicia el game loop
 vVentana = pygame.display.set_mode((vVentanaAncho, vVentanaAlto))
@@ -103,10 +127,16 @@ while ejecutando:
     vVentana.fill((0, 0, 0))
 
     dibujaCuadricula()
+    ficha.recorrerficha()
 
     pygame.display.update()
 
 
 pygame.quit()
 sys.exit()
+
+#piezas
+  #imagino que es crear un objeto 
+#que necesito?, crear el objeto, que se dibuje, que se mueva
+
 
